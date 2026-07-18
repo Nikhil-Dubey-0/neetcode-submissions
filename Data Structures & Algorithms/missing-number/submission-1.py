48b502901c917ba9f0cae890bb9016d71914e684
Nikhil Dubey <nikhildubey2thousand3@gmail.com>
2026-07-18 13:05:27 +0530

    Remove: missing-number - submission-1

--- Data Structures & Algorithms/missing-number/submission-1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        n=len(nums)
-        for i in range(n):
-            if i in nums:
-                continue
-            else:
-                return i
